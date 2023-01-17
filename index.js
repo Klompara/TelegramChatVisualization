6974692693de3ca2a6a75ed210ba69f8eac18aae
Kleinlercher Michael <36262189+Klompara@users.noreply.github.com>
2023-01-17 20:04:25 +0100

    Changed User Interface

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
-const Prompt = require('prompt-checkbox');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-function loadData() {
+async function loadData() {
     // todo file selection
-    return JSON.parse(fs.readFileSync(process.argv[2])).messages;
+    let path = await userInput('Drag and drop JSON file of Chat export:');
+    return JSON.parse(fs.readFileSync(path.replaceAll('& ', '').replaceAll('\'', ''))).messages;
 }
+
 
 function readAllMembers(data) {
     let allMembers = data.map(x => x.from).reduce(function (acc, curr) {
@@ -32,29 +38,43 @@ function prepareData(members, data) {
     return preparedData;
 }
 
-async function userSelectMembers(members) {
+async function userInput(prompt) {
     return new Promise((resolve, reject) => {
-        var prompt = new Prompt({
-            name: 'Members',
-            message: 'Which group member should be included? (navigate with arrow keys, select with space and enter to continue)',
-            choices: members
-        });
-
-        prompt.ask(function (answers) {
-            if(answers.length == 0) {
-                process.exit(0);
-            }
-            resolve(answers);
-        });
+        rl.question(prompt, (answer) => {
+            resolve(answer);
+        })
     });
 }
 
+async function selectMembers(members) {
+    let allMembers;
+    
+    while (allMembers == undefined || (allMembers != 'y' && allMembers != 'n')) {
+        allMembers = await userInput('Do you want to use all members of the chat [Y/N]? ');
+    }
+
+    if(allMembers == 'y') {
+        return members;
+    }
+
+    let selectedMembers = [];
+    for (let i = 0; i < members.length; i++) {
+        let add = undefined;
+        while (add == undefined || (add != 'y' && add != 'n')) {
+            add = await userInput('Add ' + members[i] + ' [Y/N]? ');
+        }
+        if (add == 'y') {
+            selectedMembers.push(members[i]);
+        }
+    }
+    return selectedMembers;
+}
+
 async function main() {
-    let data = loadData();
+    let data = await loadData();
     let members = readAllMembers(data);
-    members = await userSelectMembers(members);
+    members = await selectMembers(members);
     let preparedData = prepareData(members, data);
-    console.log(`preparedData`, preparedData);
 }
 
 main();
