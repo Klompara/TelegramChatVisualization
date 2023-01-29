cb8577660c5e3a0546e7b5793d1918346f710a51
Kleinlercher Michael <36262189+Klompara@users.noreply.github.com>
2023-01-29 02:07:04 +0100

    Cleanup + Average
    Added average chart (messages per day, words per message)
    minor refactor, promise all

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const rl = readline.createInterface({
 const pcj = require('phantom-chartjs');
 const chartOutputDir = './charts/'
 const colors = ['#0077b6', '#B80F0A', '#665191', '#a05195', '#d45087', '#f95d6a', '#ff7c43', '#ffa600'];
+const topUsedWordsCount = 20;
+
+/*
+TODO
+    emoji
+    top words
+    chart background
+    display labels
+    group calls
+    media (video, photo...)
+*/
 
 Date.prototype.addDays = function (days) {
     let date = new Date(this.valueOf());
@@ -22,7 +33,17 @@ pcj.createChartRenderer({ port: 8080 }, (err, renderer) => {
             fs.mkdirSync(chartOutputDir);
         }
 
-        fs.writeFileSync(chartOutputDir + filename + '.jpg', base64, 'base64', (err) => { console.error('Error writing file: ' + err) });
+        fs.writeFileSync(chartOutputDir + filename + '.png', base64, 'base64', (err) => { console.error('Error writing file: ' + err) });
+    }
+
+    function render(config, name, resolve, reject) {
+        renderer.renderBase64(config, function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({ chartName: name, data: data });
+            }
+        });
     }
 
     async function loadData() {
@@ -30,7 +51,6 @@ pcj.createChartRenderer({ port: 8080 }, (err, renderer) => {
         let path = await userInput('Drag and drop JSON file of Chat export:');
         return JSON.parse(fs.readFileSync(path.replaceAll('& ', '').replaceAll('\'', '').replaceAll('"', ''))).messages;
     }
-
 
     function readAllMembers(data) {
         let allMembers = data.map(x => x.from).reduce(function (acc, curr) {
@@ -106,193 +126,255 @@ pcj.createChartRenderer({ port: 8080 }, (err, renderer) => {
     }
 
     function chartTotalWordCount(chatData) {
-        let labels = chatData.map(x => x.name);
-        let data = chatData.map(x => x.allWords.length);
-        let config = {
-            width: 1920,
-            chart: {
-                type: 'bar',
-                data: {
-                    labels: labels,
-                    datasets: [{
-                        label: 'Total Word Count',
-                        data: data,
-                        backgroundColor: colors
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
+        return new Promise((resolve, reject) => {
+            let labels = chatData.map(x => x.name);
+            let data = chatData.map(x => x.allWords.length);
+            let config = {
+                width: 1920,
+                chart: {
+                    type: 'bar',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: 'Total Word Count',
+                            data: data,
+                            backgroundColor: colors
                         }]
-                    }
-                }
-            }
-        };
-
-        renderer.renderBase64(config, function (err, data) { saveBase64File(data, 'words'); console.log(`Finished total words bar chart!`); });
-    }
-
-    function chartTotalMessageCount(chatData) {
-        let labels = chatData.map(x => x.name);
-        let data = chatData.map(x => x.messages.length);
-        let config = {
-            width: 1920,
-            chart: {
-                type: 'bar',
-                data: {
-                    labels: labels,
-                    datasets: [{
-                        label: 'Total Message Count',
-                        data: data,
-                        backgroundColor: colors
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            }
-        };
-
-        renderer.renderBase64(config, function (err, data) { saveBase64File(data, 'messages'); console.log(`Finished total messages bar chart!`); });
-    }
-
-    function chartWeekDay(chatData) {
-        let labels = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        let dataSets = [];
-        for (let i = 0; i < chatData.length; i++) {
-            let counter = labels.map(x => { return { weekday: x, count: 0 } });
-            for (let j = 0; j < chatData[i].messages.length; j++) {
-                let weekday = labels[new Date(chatData[i].messages[j].date_unixtime * 1000).getUTCDay()]
-                counter.find(x => x.weekday == weekday).count++;
-            }
-
-            dataSets.push({
-                label: chatData[i].name,
-                data: counter.map(x => x.count),
-                borderColor: colors[i]
-            })
-        }
-        let config = {
-            width: 1920,
-            chart: {
-                type: 'radar',
-                data: {
-                    labels: labels,
-                    datasets: dataSets
-                },
-                options: {
-                    scale: {
-                        ticks: {
-                            beginAtZero: true
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
                         }
                     }
                 }
-            }
-        };
+            };
 
-        renderer.renderBase64(config, function (err, data) { saveBase64File(data, 'week'); console.log(`Finished week radar chart!`); });
+            render(config, 'words', resolve, reject);
+        });
+    }
+
+    function chartTotalMessageCount(chatData) {
+        return new Promise((resolve, reject) => {
+            let labels = chatData.map(x => x.name);
+            let total = chatData.map(x => x.messages.length);
+            let config = {
+                width: 1920,
+                chart: {
+                    type: 'bar',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: 'Total Messages',
+                            data: total,
+                            backgroundColor: colors
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                }
+            };
+
+            render(config, 'messages', resolve, reject);
+        });
+    }
+
+    function chartAverageMessageCount(chatData) {
+        return new Promise((resolve, reject) => {
+            let labels = chatData.map(x => x.name);
+            let averageMessagesDay = chatData.map(function (x) {
+                let messages = x.messages.length;
+                let days = x.messages.map(y => new Date(new Date(y.date_unixtime * 1000).toDateString()).getTime());
+                days = days.filter((y, index) => days.indexOf(y) == index).length;
+                return messages / days;
+            });
+            let averageWordsMessage = chatData.map(x => x.allWords.length / x.messages.length);
+            let config = {
+                width: 1920,
+                chart: {
+                    type: 'bar',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: 'Average messages per day',
+                            data: averageMessagesDay,
+                            backgroundColor: colors[0]
+                        }, {
+                            label: 'Average words per message',
+                            data: averageWordsMessage,
+                            backgroundColor: colors[1]
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                }
+            };
+
+            render(config, 'average', resolve, reject);
+        });
+    }
+
+    function chartWeekDay(chatData) {
+        return new Promise((resolve, reject) => {
+            let labels = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+            let dataSets = [];
+            for (let i = 0; i < chatData.length; i++) {
+                let counter = labels.map(x => { return { weekday: x, count: 0 } });
+                for (let j = 0; j < chatData[i].messages.length; j++) {
+                    let weekday = labels[new Date(chatData[i].messages[j].date_unixtime * 1000).getUTCDay()]
+                    counter.find(x => x.weekday == weekday).count++;
+                }
+
+                dataSets.push({
+                    label: chatData[i].name,
+                    data: counter.map(x => x.count),
+                    borderColor: colors[i]
+                })
+            }
+            let config = {
+                width: 1920,
+                chart: {
+                    type: 'radar',
+                    data: {
+                        labels: labels,
+                        datasets: dataSets
+                    },
+                    options: {
+                        scale: {
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                }
+            };
+
+            render(config, 'week', resolve, reject);
+        });
     }
 
     function chartFullLine(chatData) {
-        let labels = [];
-        let dataSets = [];
-        let minTime = 8640000000000000;
-        let maxTime = -8640000000000000;
-        for (let i = 0; i < chatData.length; i++) {
-            let counter = [];
-            for (let j = 0; j < chatData[i].messages.length; j++) {
-                let unixTime = chatData[i].messages[j].date_unixtime * 1000;
+        return new Promise((resolve, reject) => {
+            let labels = [];
+            let dataSets = [];
+            let minTime = 8640000000000000;
+            let maxTime = -8640000000000000;
+            for (let i = 0; i < chatData.length; i++) {
+                let counter = [];
+                for (let j = 0; j < chatData[i].messages.length; j++) {
+                    let unixTime = chatData[i].messages[j].date_unixtime * 1000;
 
-                let messageDay = new Date(new Date(unixTime).toDateString()).toDateString();
-                if (counter.find(x => x.day === messageDay) == undefined) {
-                    counter.push({ day: messageDay, count: 1 });
-                } else {
-                    counter.find(x => x.day === messageDay).count++;
+                    let messageDay = new Date(new Date(unixTime).toDateString()).toDateString();
+                    if (counter.find(x => x.day === messageDay) == undefined) {
+                        counter.push({ day: messageDay, count: 1 });
+                    } else {
+                        counter.find(x => x.day === messageDay).count++;
+                    }
+
+                    // get min and max time
+                    minTime = minTime > unixTime ? unixTime : minTime;
+                    maxTime = maxTime < unixTime ? unixTime : maxTime;
                 }
 
-                // get min and max time
-                minTime = minTime > unixTime ? unixTime : minTime;
-                maxTime = maxTime < unixTime ? unixTime : maxTime;
+                dataSets.push({
+                    label: chatData[i].name,
+                    data: counter.map(x => x.count),
+                    borderColor: colors[i]
+                });
             }
 
-            dataSets.push({
-                label: chatData[i].name,
-                data: counter.map(x => x.count),
-                borderColor: colors[i]
-            });
-        }
+            //generate labels
+            let date = new Date(new Date(minTime).toDateString());
+            let dateMax = new Date(new Date(maxTime).toDateString());
+            while (date <= dateMax) {
+                labels.push(new Date(date.toDateString()).toDateString());
+                date = date.addDays(1);
+            }
 
-        //generate labels
-        let date = new Date(new Date(minTime).toDateString());
-        let dateMax = new Date(new Date(maxTime).toDateString());
-        while (date <= dateMax) {
-            labels.push(new Date(date.toDateString()).toDateString());
-            date = date.addDays(1);
-        }
-
-        let config = {
-            width: 1920,
-            chart: {
-                type: 'line',
-                data: {
-                    labels: labels,
-                    datasets: dataSets
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            stacked: false
-                        }]
+            let config = {
+                width: 1920,
+                chart: {
+                    type: 'line',
+                    data: {
+                        labels: labels,
+                        datasets: dataSets
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                stacked: false
+                            }]
+                        }
                     }
                 }
-            }
-        };
+            };
 
-        renderer.renderBase64(config, function (err, data) { saveBase64File(data, 'fullLine'); console.log(`Finished history line chart!`); });
+            render(config, 'history', resolve, reject);
+        });
     }
 
     function chartMostUsedWords(chatData) {
-        for (let i = 0; i < chatData.length; i++) {
-            let wordlist = chatData[i].allWords;
-            let occurences = [];
-            for (let j = 0; j < wordlist.length; j++) {
-                let word = wordlist[j];
-                if (occurences.find(x => x.word == word) == undefined) {
-                    occurences.push({ word: word, count: 1 });
-                } else {
-                    occurences.find(x => x.word == word).count++;
+        return new Promise((resolve, reject) => {
+            for (let i = 0; i < chatData.length; i++) {
+                let wordlist = chatData[i].allWords;
+                let occurences = [];
+                for (let j = 0; j < wordlist.length; j++) {
+                    let word = wordlist[j];
+                    if (occurences.find(x => x.word == word) == undefined) {
+                        occurences.push({ word: word, count: 1 });
+                    } else {
+                        occurences.find(x => x.word == word).count++;
+                    }
                 }
+                occurences = occurences.sort(function (a, b) {
+                    return b.count - a.count;
+                });
+                console.log(occurences);
             }
-            occurences = occurences.sort(function (a, b) {
-                return b.count - a.count;
-            });
-            console.log(occurences);
-        }
+        });
     }
 
-    async function main() {
+    (async function main() {
         let data = await loadData();
         let members = readAllMembers(data);
         members = await selectMembers(members);
         let preparedData = prepareUserData(members, data);
         let groupCalls = prepareGroupCalls(data);
-        chartTotalWordCount(preparedData);
-        chartTotalMessageCount(preparedData);
-        chartWeekDay(preparedData);
-        chartFullLine(preparedData);
-        chartMostUsedWords(preparedData);
-    }
-
-    main()
+        Promise.all([
+            chartTotalWordCount(preparedData),
+            chartTotalMessageCount(preparedData),
+            chartAverageMessageCount(preparedData),
+            chartWeekDay(preparedData),
+            chartFullLine(preparedData)]
+        ).then(data => {
+            data.forEach(x => {
+                saveBase64File(x.data, x.chartName);
+                console.log(`Saved chart: ${x.chartName}`);
+            });
+        }).catch(err => {
+            console.error(`Error Generating Chart!`, err);
+        }).finally(() => {
+            renderer.close();
+            process.exit(0);
+        });
+    }())
 });
 
 
